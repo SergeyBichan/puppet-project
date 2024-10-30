c28955dab92f74ca5e7e660ffb849a8899f493d5
SergeyBichan <ugidancer@gmail.com>
2024-10-30 13:50:42 +0300

    add SLF4J and logs

--- src/main/java/com/aston/frontendpracticeservice/controller/UserController.java
+++ src/main/java/com/aston/frontendpracticeservice/controller/UserController.java
@@ -4,15 +4,19 @@ import com.aston.frontendpracticeservice.domain.dto.UserDto;
 import com.aston.frontendpracticeservice.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/users")
@@ -22,21 +26,24 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<UserDto> getUserById(@PathVariable("id") @Valid Long id) {
-        UserDto byId = service.findById(id);
-        return new ResponseEntity<>(byId, HttpStatus.OK);
+        UserDto dto = service.findById(id);
+        log.info("Get request getUserById - start: {}", dto);
+        return new ResponseEntity<>(dto, HttpStatus.OK);
     }
 
     @GetMapping("/all")
     public ResponseEntity<List<UserDto>> getAllUsers(){
         List<UserDto> all = service.findAll();
+        log.info("Get request getAllUsers - start: {}", all);
         return new ResponseEntity<>(all, HttpStatus.OK);
     }
 
-    @GetMapping("/{firstName}/{lastName}")
-    public ResponseEntity<UserDto> getUserByLogin(
-            @PathVariable("firstName") @Valid String firstName,
-            @PathVariable("lastName") @Valid String lastName) {
+    @GetMapping("/by-name")
+    public ResponseEntity<UserDto> getUserByFirstAndLastName(
+            @RequestParam("firstName") @Valid String firstName,
+            @RequestParam("lastName") @Valid String lastName) {
         UserDto dto = service.findByFirstAndLastName(firstName, lastName);
+        log.info("Get request getUserByFirstAndLastName - start: {}", dto);
         return new ResponseEntity<>(dto, HttpStatus.OK);
     }
 }
