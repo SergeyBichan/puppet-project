83e401a6fecdb9a86131322149136429b38b94e9
SergeyBichan <ugidancer@gmail.com>
2024-10-30 12:01:57 +0300

    add User.java

--- src/main/java/com/aston/frontendpracticeservice/domain/entity/User.java
+++ src/main/java/com/aston/frontendpracticeservice/domain/entity/User.java
@@ -1,19 +1,50 @@
 package com.aston.frontendpracticeservice.domain.entity;
 
 import com.aston.frontendpracticeservice.security.Role;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.time.LocalDate;
 import java.util.Set;
 
 @Getter
 @Setter
 @Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "first_name")
+    private String firstName;
+
+    @Column(nullable = false, name = "last_name")
+    private String lastName;
+
+    @Column(nullable = false, name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(nullable = false, name = "inn")
+    private String inn;
+
+    @Column(nullable = false, name = "snils")
+    private String snils;
+
+    @Column(nullable = false, name = "passport_number")
+    private String passportNumber;
+
+    @Column(nullable = false, name = "login")
     private String login;
 
+    @Column(nullable = false, name = "password")
     private String password;
 
+    @Transient
     private Set<Role> roles;
+
 }
