f55a80d36f5775ae3d2d2a400f2de9e9d4d5aad8
SergeyBichan <ugidancer@gmail.com>
2024-10-31 14:05:13 +0300

    Заменил на sql скрипт, согласно замечаний по ревью не в тестовой папке

--- src/main/resources/db/changelog/changeset/sql/create-user-table.sql
+++ src/main/resources/db/changelog/changeset/sql/create-user-table.sql
@@ -1,1 +1,11 @@
-CREATE TABLE users
+CREATE TABLE users (
+                       id SERIAL PRIMARY KEY,
+                       first_name VARCHAR(255) NOT NULL,
+                       last_name VARCHAR(255) NOT NULL,
+                       birth_date DATE NOT NULL,
+                       inn VARCHAR(12) NOT NULL UNIQUE,
+                       snils VARCHAR(11) NOT NULL UNIQUE,
+                       passport_number VARCHAR(20) NOT NULL UNIQUE,
+                       login VARCHAR(50) NOT NULL UNIQUE,
+                       password VARCHAR(255) NOT NULL
+);
