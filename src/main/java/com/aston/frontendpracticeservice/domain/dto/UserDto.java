13042525e20ed2f1143a997570d4e785b56c8fec
SergeyBichan <ugidancer@gmail.com>
2024-10-31 10:33:04 +0300

    optimized imports

--- src/main/java/com/aston/frontendpracticeservice/domain/dto/UserDto.java
+++ src/main/java/com/aston/frontendpracticeservice/domain/dto/UserDto.java
@@ -1,6 +1,10 @@
 package com.aston.frontendpracticeservice.domain.dto;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,7 +22,7 @@ public class UserDto {
     @NotBlank(message = "Last name can't be blank")
     private String lastName;
     @NotNull(message = "Дата не может быть пустой")
-    @Future(message = "Неверная дата")
+    @Past(message = "Неверная дата")
     private LocalDate birthDate;
     @NotBlank(message = "ИНН не может быть пустым")
     @Pattern(regexp = "\\{12}", message = "ИНН должен содержать не менее 12 цифр")
