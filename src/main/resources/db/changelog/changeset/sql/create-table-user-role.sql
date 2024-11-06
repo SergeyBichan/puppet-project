225cead85b90623909cfbfcee280cab24ffd5710
SergeyBichan <ugidancer@gmail.com>
2024-11-06 11:55:43 +0300

    add sql script with creating user_roles table

--- src/main/resources/db/changelog/changeset/sql/create-table-user-role.sql
+++ src/main/resources/db/changelog/changeset/sql/create-table-user-role.sql
@@ -1,4 +1,4 @@
-CREATE TABLE user_role(
+CREATE TABLE user_roles(
     user_id INT NOT NULL,
     user_role VARCHAR(30) NOT NULL,
     CONSTRAINT fk_user_role FOREIGN KEY (user_id) REFERENCES users(id)
