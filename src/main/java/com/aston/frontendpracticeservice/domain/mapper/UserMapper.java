0d6507dbc6813a39ef2694ed91fb82e755e0bf3f
SergeyBichan <ugidancer@gmail.com>
2024-11-06 11:57:30 +0300

    add default scripts

--- src/main/java/com/aston/frontendpracticeservice/domain/mapper/UserMapper.java
+++ src/main/java/com/aston/frontendpracticeservice/domain/mapper/UserMapper.java
@@ -2,12 +2,22 @@ package com.aston.frontendpracticeservice.domain.mapper;
 
 import com.aston.frontendpracticeservice.domain.dto.UserDto;
 import com.aston.frontendpracticeservice.domain.entity.User;
+import com.aston.frontendpracticeservice.security.Role;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.Set;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+//    @Mapping(target = "roles", expression = "java(setDefaultRole())")
     UserDto toDto(User entity);
 
+//    @Mapping(target = "roles", expression = "java(setDefaultRole())")
     User toUser(UserDto dto);
+
+    default Set<Role> setDefaultRole() {
+      return Set.of(Role.USER);
+    }
 }
