ac746a6337f8f49d571d5767e89cdbb5ddec7169
SergeyBichan <ugidancer@gmail.com>
2024-11-06 11:57:13 +0300

    add EntityGraph annotation to repository

--- src/main/java/com/aston/frontendpracticeservice/repository/UserRepository.java
+++ src/main/java/com/aston/frontendpracticeservice/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.aston.frontendpracticeservice.repository;
 
 import com.aston.frontendpracticeservice.domain.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 
@@ -11,7 +14,10 @@ import java.util.Optional;
  */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
     Optional<User> findByLogin(String login);
+    @EntityGraph(value = "roles", type = EntityGraph.EntityGraphType.LOAD)
     Optional<User> findByFirstNameAndLastName(String firstName, String lastName);
+
+    @EntityGraph(value = "roles", type = EntityGraph.EntityGraphType.LOAD)
+    List<User> findAll();
 }
