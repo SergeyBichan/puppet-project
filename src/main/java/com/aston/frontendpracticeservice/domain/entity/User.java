797a31128074f1bb68993d11c1d9a26e6e375498
Sergey Bichan <s.bichan@astondevs.ru>
2024-11-08 16:06:46 +0300

    поправил согласно замечаниям техлида

--- src/main/java/com/aston/frontendpracticeservice/domain/entity/User.java
+++ src/main/java/com/aston/frontendpracticeservice/domain/entity/User.java
@@ -2,15 +2,11 @@ package com.aston.frontendpracticeservice.domain.entity;
 
 import com.aston.frontendpracticeservice.security.Role;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
+
 import java.time.LocalDate;
-import java.util.Objects;
 import java.util.Set;
 
 @Getter
@@ -19,6 +15,7 @@ import java.util.Set;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 @Table(name = "users")
 @NamedEntityGraph(name = "roles",
         attributeNodes = @NamedAttributeNode("roles"))
@@ -57,36 +54,5 @@ public class User {
     @Enumerated(EnumType.STRING)
     @Column(name = "user_role")
     @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
-
     private Set<Role> roles;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(firstName, user.firstName)
-                && Objects.equals(lastName, user.lastName)
-                && Objects.equals(birthDate, user.birthDate)
-                && Objects.equals(inn, user.inn)
-                && Objects.equals(snils, user.snils)
-                && Objects.equals(passportNumber, user.passportNumber)
-                && Objects.equals(login, user.login)
-                && Objects.equals(password, user.password)
-                && Objects.equals(roles, user.roles);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id,
-                firstName,
-                lastName,
-                birthDate,
-                inn,
-                snils,
-                passportNumber,
-                login,
-                password,
-                roles);
-    }
 }
