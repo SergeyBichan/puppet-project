8d784077d9a8b1a1cb1a4159ed9a7dc5e6b9d40c
SergeyBichan <ugidancer@gmail.com>
2024-11-11 16:08:01 +0300

    Отрефакторил и добавил sql скрипты для тестовой бд

---
+++ src/main/resources/db/changelog/changeset/sql/create-requisites-table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE requisites (
+                            id SERIAL PRIMARY KEY,
+                            account_number VARCHAR(255),
+                            correspondent_account_number VARCHAR(255),
+                            inn VARCHAR(12),
+                            kpp VARCHAR(9),
+                            kbk VARCHAR(20),
+                            user_id BIGINT,
+                            FOREIGN KEY (user_id) REFERENCES users(id)
+);
---
+++ src/test/resources/db/changelog/changeset/sql/add-test-requisites-test.sql
@@ -0,0 +1,9 @@
+INSERT INTO requisites (account_num, correspondent_account_number, inn, kpp, kbk, user_id)
+VALUES (
+            'fds',
+        'fds',
+        '123461234562',
+        'fds',
+        'fds',
+        1
+       )
