86356af936106a24d2c768b3139b0946575a8d60
SergeyBichan <ugidancer@gmail.com>
2024-11-11 17:25:48 +0300

    Добавил ендпоинт

---
+++ src/main/java/com/aston/frontendpracticeservice/controller/RequisiteController.java
@@ -0,0 +1,27 @@
+package com.aston.frontendpracticeservice.controller;
+
+import com.aston.frontendpracticeservice.domain.dto.RequisitesProjection;
+import com.aston.frontendpracticeservice.service.RequisiteService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/requisites")
+public class RequisiteController {
+
+    private final RequisiteService requisiteService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RequisitesProjection> getRequisitesByUser(@PathVariable("id") Long id){
+        RequisitesProjection requisitesProjection = requisiteService.getRequisites(id);
+        return new ResponseEntity<>(requisitesProjection, HttpStatus.OK);
+    }
+}
--- src/main/java/com/aston/frontendpracticeservice/service/RequisiteService.java
+++ src/main/java/com/aston/frontendpracticeservice/service/RequisiteService.java
@@ -5,12 +5,17 @@ import com.aston.frontendpracticeservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @RequiredArgsConstructor
 @Service
 public class RequisiteService {
     private final UserRepository repository;
 
     public RequisitesProjection getRequisites(Long id) {
+        if (Objects.isNull(id) || id == 0) {
+            throw new RuntimeException("Incorrect format");
+        }
         return repository.findUserWithRequisites(id);
     }
 }
