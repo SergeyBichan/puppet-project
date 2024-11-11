d6bd31e71c1e2348646d4c49ddf4d50e31d75a63
SergeyBichan <ugidancer@gmail.com>
2024-11-11 15:53:50 +0300

    Добавил метод на получение реквизитов из бд

--- src/main/java/com/aston/frontendpracticeservice/repository/UserRepository.java
+++ src/main/java/com/aston/frontendpracticeservice/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.aston.frontendpracticeservice.repository;
 
+import com.aston.frontendpracticeservice.domain.dto.RequisitesProjection;
 import com.aston.frontendpracticeservice.domain.entity.User;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -20,4 +22,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @EntityGraph(value = "roles", type = EntityGraph.EntityGraphType.LOAD)
     List<User> findAll();
+
+    @Query("SELECT new com.aston.frontendpracticeservice.domain.dto.RequisitesProjection" +
+            "(u.firstName, u.lastName, r.accountnum, r.kbk) " +
+            "FROM Requisites r JOIN r.user u WHERE u.id=:id")
+    RequisitesProjection findUserWithRequisites(Long id);
 }
