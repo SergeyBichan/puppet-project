97805d1f4bc93abd4010baeef3a54d25f869ef36
SergeyBichan <ugidancer@gmail.com>
2024-11-11 16:10:53 +0300

    Поправил импорты

--- src/main/java/com/aston/frontendpracticeservice/domain/entity/Requisites.java
+++ src/main/java/com/aston/frontendpracticeservice/domain/entity/Requisites.java
@@ -1,7 +1,20 @@
 package com.aston.frontendpracticeservice.domain.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 
 @Getter
 @Setter
